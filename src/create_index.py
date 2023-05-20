25c6212e46ec165dd15341fa168966f929625a37
Santosh Marigowda <santosharakere@gmail.com>
2023-05-20 08:21:23 +0100

    ingest and create index

--- src/create_index.py
+++ src/create_index.py
@@ -7,6 +7,7 @@ pinecone_environment = os.environ.get("PINECONE_ENVIRONMENT")
 
 def main():
     pinecone.init(api_key=pinecone_api_key, environment=pinecone_environment)
+    pinecone.create_index("pinecone-demo-index", metric="cosine", dimension=1056)
     pass
 
 

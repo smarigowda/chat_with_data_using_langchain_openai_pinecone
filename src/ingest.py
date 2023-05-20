c6a58392d21bdbd0d99d2a6b3e8b7b5d6752eaf3
Santosh Marigowda <santosharakere@gmail.com>
2023-05-20 09:29:57 +0100

    pdf doc load success

--- src/ingest.py
+++ src/ingest.py
@@ -1,5 +1,6 @@
 import os
 import pinecone
+from langchain.document_loaders import DirectoryLoader
 
 pinecone_api_key = os.environ.get("PINECONE_API_KEY")
 pinecone_environment = os.environ.get("PINECONE_ENVIRONMENT")
@@ -7,6 +8,8 @@ pinecone_environment = os.environ.get("PINECONE_ENVIRONMENT")
 
 def main():
     pinecone.init(api_key=pinecone_api_key, environment=pinecone_environment)
+    loader = DirectoryLoader("./docs", glob="**/*.pdf")
+    docs = loader.load()
     pass
 
 
